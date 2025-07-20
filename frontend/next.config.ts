18ab6d137184cfbc86080ce5ffc213bbc0fecb76
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-07-20 16:31:19 +0300

    fix destination path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://backend:8080/api/:path*", // имя сервиса из docker-compose
+      },
+    ];
+  },
 };
 
 export default nextConfig;
