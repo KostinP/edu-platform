3f76bb8da18b15f1d7bc0f7e58113c60b641df4c
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-07-20 12:43:41 +0300

    fix deploy.sh

---
+++ frontend/deploy.sh
@@ -0,0 +1,21 @@
+# Скрипт автоматизирует сборку фронтенда и передачу с клиента на сервер
+# chmod +x deploy.sh
+# ./deploy.sh
+
+#!/bin/bash
+
+# Установка зависимостей
+npm install
+
+# Сборка фронтенда
+npm run build
+
+# Отправляем исходники на сервер
+rsync -avz --delete . root@213.139.208.67:/srv/edu-platform/frontend/
+
+# На сервере пересобираем образ и перезапускаем контейнер
+ssh root@213.139.208.67 << EOF
+  cd /srv/edu-platform
+  docker-compose build frontend
+  docker-compose up -d frontend
+EOF
